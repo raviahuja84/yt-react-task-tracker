2f5913fe927131ded0b9bfd691cb02e96f3d376f
Ravi Ahuja <ravi.ahuja@medisecure.com.au>
2023-01-09 09:54:26 +1100

    Cleanup + reorg

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,22 +1,53 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useRef } from 'react';
 
 const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
   const [showAddTask, SetShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
+  const effectRan = useRef(false);
 
   useEffect(() => {
-    // Get Tasks
-    const getTasks = async () => {
-      const data = await fetch('http://localhost:5000/tasks').then((resp) =>
-        resp.json()
-      );
+    console.log('useEffect Ran');
 
-      setTasks(data);
+    // Avoids the API fetch call twice on dev mode
+    // variation #1 - use api data from 1st fetch
+    /*     if (effectRan.current === false) {
+      // Get Tasks
+      const getTasks = async () => {
+        const data = await fetch('http://localhost:5000/tasks').then((resp) =>
+          resp.json()
+        );
+        console.log(data);
+        setTasks(data);
+      };
+
+      getTasks();
+
+      return () => {
+        console.log('UseEffect Unmounted');
+        effectRan.current = true;
+      };
+    }
+ */
+    // Variation #2 - use 2nd fetch data
+    if (effectRan.current === true) {
+      // Get Tasks
+      const getTasks = async () => {
+        const data = await fetch('http://localhost:5000/tasks').then((resp) =>
+          resp.json()
+        );
+        console.log(data);
+        setTasks(data);
+      };
+
+      getTasks();
+    }
+
+    return () => {
+      console.log('UseEffect Unmounted');
+      effectRan.current = true;
     };
-    console.log('useEffect called from context');
-    getTasks();
   }, []);
 
   // Toggle Add task
